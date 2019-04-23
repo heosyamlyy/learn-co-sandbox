c1ba36369d5b78f1132d9be71814667b32a51b5e
Yangyang Liu <heosyamlyy@gmail.com>
2019-04-23 04:59:17 +0000

    Done.

---
+++ ConditionalStatement.js
@@ -0,0 +1,53 @@
+if (5 > 2) {
+  console.log(" 5 is greater than 2")
+  
+}
+
+if (2 > 5) {
+  console.log("2 is greater than 5")
+}
+
+if (false) {
+  console.log("This will never get printed because the above statement evaluates to false.")
+} else  {
+  console.log("This will get printed!")
+}
+
+if (6 + 3 === 9) {
+  console.log("Giraffes have no vocal cords.")
+  
+}
+
+if (6 + 3 < 5) {
+  console.log("The hummingbird is the only animal that can fly backwards")
+}
+
+var dog = "satisfied"
+if (dog === "hungry") {
+  console.log("Refilling food bowl.")
+  
+} else {
+  console.log("Reading newspaper.")
+}var dog = "thirsty"
+ 
+if (dog === "hungry") {
+  console.log("Refilling food bowl.")
+} else if (dog === "thirsty") {
+  console.log("Refilling water bowl.")
+} else {
+  console.log("Reading newspaper.")
+}
+
+var dog = "cuddly"
+if (dog === "hungry") {
+  console.log("Refilling food bowl.")
+ 
+} else if (dog === "thirsty") {
+  console.log("Refilling water bowl.")
+ 
+} else if (dog === "playful") {
+  console.log("Playing tug-of-war.")
+ 
+} else if (dog === "cuddly") {
+  console.log("Snuggling.")
+}
