9d8307ce9ea87e8ffcb3bd38ccfede08f2b40cdd
Yangyang Liu <heosyamlyy@gmail.com>
2019-04-23 04:58:44 +0000

    Automatically backed up by Learn

--- ConditionalStatement.js
+++ ConditionalStatement.js
@@ -40,5 +40,14 @@ if (dog === "hungry") {
 
 var dog = "cuddly"
 if (dog === "hungry") {
-  
+  console.log("Refilling food bowl.")
+ 
+} else if (dog === "thirsty") {
+  console.log("Refilling water bowl.")
+ 
+} else if (dog === "playful") {
+  console.log("Playing tug-of-war.")
+ 
+} else if (dog === "cuddly") {
+  console.log("Snuggling.")
 }
