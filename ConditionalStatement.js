f72a2db09a2ef1ee5e9b700b44349653f549010b
Yangyang Liu <heosyamlyy@gmail.com>
2019-04-23 04:56:43 +0000

    Automatically backed up by Learn

--- ConditionalStatement.js
+++ ConditionalStatement.js
@@ -2,3 +2,35 @@ if (5 > 2) {
   console.log(" 5 is greater than 2")
   
 }
+
+if (2 > 5) {
+  console.log("2 is greater than 5")
+}
+
+if (false) {
+  console.log("This will never get printed because the above statement evaluates to false.")
+} else  {
+  console.log("This will get printed!")
+}
+
+if (6 + 3 === 9) {
+  console.log("Giraffes have no vocal cords.")
+  
+}
+
+if (6 + 3 < 5) {
+  console.log("The hummingbird is the only animal that can fly backwards")
+}
+
+var dog = "satisfied"
+if (dog === "hungry") {
+  console.log("Refilling food bowl.")
+  
+} else {
+  console.log("Reading newspaper.")
+}
+
+var dog = "thirsty"
+if (dog === "hungry") {
+  console.log("re")
+}
